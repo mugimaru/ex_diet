74e4c72c016b4ae2f386fa928c90398c9c4f0a94
mugimaru73 <Mugimaru@live.ru>
2018-04-23 17:52:45 +0400

    update deploy script

--- .deliver/deploy.sh
+++ .deliver/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-mix edeliver build release --verbose
-mix edeliver deploy release to production --verbose
-mix edeliver start production --verbose
-mix edeliver migrate production up --verbose
+mix edeliver build release --verbose --plain
+mix edeliver deploy release to production --verbose --plain
+mix edeliver restart production --verbose --plain
+mix edeliver migrate production up --verbose --plain
