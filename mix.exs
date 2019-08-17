b0a7d8b5f9bc5190307722061f77ee65baf2536e
n.babushkin <n.babushkin@fun-box.ru>
2019-08-17 06:59:30 +0400

    elixir 1.9.1 & erlang 22

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule ExDiet.Mixfile do
   def project do
     [
       app: :ex_diet,
-      version: "0.0.1",
-      elixir: "~> 1.4",
+      version: "0.0.2",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
