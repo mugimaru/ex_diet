e0d971f0896bf051d7f158a117234c55ccc97748
mugimaru73 <Mugimaru@live.ru>
2020-05-05 16:29:37 +0400

    build releases in docker instead of remote host

--- .deliver/deploy.sh
+++ .deliver/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -ex
 
-mix edeliver build release --verbose
 mix edeliver deploy release to production --verbose
 mix edeliver restart production --verbose
 mix edeliver migrate production up --verbose
