30b05e942d6aedc31aa861dab9f70bc9d9d4cd32
mugimaru73 <Mugimaru@live.ru>
2020-09-12 22:22:47 +0300

    bump version 0.2.0 → 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExDiet.Mixfile do
   def project do
     [
       app: :ex_diet,
-      version: "0.2.0",
+      version: "1.0.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
