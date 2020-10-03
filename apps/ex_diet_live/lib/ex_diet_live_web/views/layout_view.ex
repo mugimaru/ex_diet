f223c83e4e56a71608676fddd8e0d405598e5b1d
mugimaru73 <Mugimaru@live.ru>
2020-10-04 01:07:54 +0400

    migrate to bootstrap5

--- apps/ex_diet_live/lib/ex_diet_live_web/views/layout_view.ex
+++ apps/ex_diet_live/lib/ex_diet_live_web/views/layout_view.ex
@@ -1,35 +1,15 @@
 defmodule ExDietLiveWeb.LayoutView do
   use ExDietLiveWeb, :view
 
-  def render_navbar(conn, opts \\ []) do
-    content_tag(:ul, class: Keyword.get(opts, :wrapper_class, ""), id: Keyword.get(opts, :wrapper_id, "")) do
-      navs_for_user(conn, conn.assigns[:current_user])
-    end
-  end
-
   def navbar_pages do
     %{
       "Ingredients" => Routes.ingredient_path(ExDietLiveWeb.Endpoint, :index)
     }
   end
 
-  defp navs_for_user(conn, nil) do
-    [
-      navbar_link(conn, "Login", Routes.account_session_path(conn, :new))
-    ]
-  end
-
-  defp navs_for_user(conn, %ExDiet.Accounts.User{}) do
-    pages = for {title, path} <- navbar_pages(), do: navbar_link(conn, title, path)
-    pages ++ [navbar_link(conn, "Logout", Routes.account_session_path(conn, :delete), method: :delete)]
-  end
-
-  defp navbar_link(conn, title, path, opts \\ []) do
-    class = if active_nav?(conn, path), do: "active", else: ""
-
-    content_tag(:li, class: class) do
-      link(title, to: path, method: Keyword.get(opts, :method, :get))
-    end
+  def navbar_link(conn, title, path, opts \\ []) do
+    active = if active_nav?(conn, path), do: "active", else: ""
+    link(title, to: path, class: "nav-link #{active}", method: Keyword.get(opts, :method, :get))
   end
 
   defp active_nav?(conn, path) do
